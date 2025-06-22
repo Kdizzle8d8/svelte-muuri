f3825ac9c7f78cfc579c247113b598b6cd411b6d
Kyle Davis <kyle@kyle.stream>
2025-06-21 20:04:16 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="w-screen h-screen flex bg-slate-900">
+	<div class="flex items-center justify-center w-full h-full">
+		<h1 class="text-white text-4xl font-bold">Hello World</h1>
+	</div>
+</div>
